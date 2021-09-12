123c7516d7871b0ee9ace2d227e6135902d9aacc
Pedro Pinto da Silva <pedro.silva@collabora.com>
2021-09-12 12:10:23 +0200

    Generate.sh: Improve file manipulation and Refactoring
    Refactoring
    - File creation moved to the top
    - Fix comment typos
    
    Improve files manipulation
    - Create Temporary file so to avoid havving duplicated
    HTML tags (HTML, BODY etc) whithin inner html
      - Remove those tags
      - And when remove temporary file at the end
    - Fix echo

--- generate.sh
+++ generate.sh
@@ -8,6 +8,10 @@
 # source ./venv/bin/activate
 # pip install odfpy
 
+FILENAME="$1"
+
+[ -z "$FILENAME" ] && read -p "Missing ODT filename. Enter it without file extension: " FILENAME
+
 # Be sure we have all folders ready
 mkdir -p public/projects/
 mkdir -p public/projects/images/
@@ -21,7 +25,8 @@ cp -p static/main.css public/style/main.css
 # Copy project's images
 cp -rp content/projects/images/. public/projects/images
 
-FILENAME="$1"
+# Create new empty project page following template
+cp -frp content/projects/template -T public/projects/$FILENAME.html
 
 # CONVERT With python, --plain to get without css
 odf2xhtml --plain content/projects/$FILENAME.odt > public/projects/"${FILENAME}_converted".html
@@ -29,29 +34,30 @@ odf2xhtml --plain content/projects/$FILENAME.odt > public/projects/"${FILENAME}_
 # Replace LO pictures with images
 sed -i 's|src="Pictures/|src="images/|' public/projects/"${FILENAME}_converted".html
 
-# Do up one level in directory when placing img tags sources
+# Go up one level in directory when placing img tags sources
 sed -i 's|src="../images/|src="images/|' public/projects/"${FILENAME}_converted".html
 
-# To remove anchors from headings
+# Remove anchors from headings
 sed -i -e 's|<a id="anchor001"></a>||' public/projects/"${FILENAME}_converted".html
 
-# Duplicate, To have before and after applying index template
-cp -frp content/projects/template -T public/projects/$FILENAME.html
-
-#   -f, --force: if an existing destination file cannot be opened, remove it and try again)
-#   -p:     same as --preserve=mode,ownership,timestamps
-#   -R, -r, --recursive
+# Create temporary converted file where only inner html is kept
+# (meta, html and body tags are removed)
+cp -frp public/projects/"${FILENAME}_converted".html -T public/projects/"${FILENAME}_temp".html
+# remove first 7 lines:
+sed -i -e '1,7d' public/projects/"${FILENAME}_temp".html
+# remove last 2 lines
+sed -i '$d' public/projects/"${FILENAME}_temp".html
+sed -i '$d' public/projects/"${FILENAME}_temp".html
 
 # Adjust paths
 sed -i 's|../static/main.css|../style/main.css|' public/projects/$FILENAME.html
 
 # Modify content/projects/$FILENAME.html
 # and insert contents from index template before and after
-# https://www.linuxquestions.org/questions/linux-newbie-8/how-to-replace-string-pattern-with-multi-line-text-in-bash-script-212983/
-sed -i '/<!-- content -->/ {r '"public/projects/${FILENAME}_converted.html"'
+sed -i '/<!-- content -->/ {r '"public/projects/${FILENAME}_temp.html"'
 d;};' public/projects/$FILENAME.html
 
-# add additional class to content box
+# Add additional class to content box: 'page'
 sed -i 's|box content|box content page|' public/projects/$FILENAME.html
 
 # Append project to projects list
@@ -60,8 +66,11 @@ echo $FILENAME >> content/projects/list-ids
 # Get project's title and append it to the list of names
 perl -ln0e '$,="\n";print /(?<=<h1>).*?(?=<\/h1)/sg' public/projects/$FILENAME.html >> content/projects/list-names
 
+# Remove temporary file
+rm public/projects/"${FILENAME}_temp".html
+
 echo "👾 $(tail -n 1 content/projects/list-names)"
-echo '  ⮡ public/'$FILENAME'.html'
+echo '  ⮡ public/projects/'$FILENAME'.html'
 
 
 
